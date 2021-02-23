7f378ad3986c6aa2ceea5152db447112a3171065
Nguyen Duc Thanh <thanhnd.aptech@gmail.com>
2021-02-23 14:07:31 +0700

    commit

--- app/src/main/java/com/namviet/vtvtravel/view/f2/TravelNewsActivity.java
+++ app/src/main/java/com/namviet/vtvtravel/view/f2/TravelNewsActivity.java
@@ -3,16 +3,25 @@ package com.namviet.vtvtravel.view.f2;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.namviet.vtvtravel.R;
 import com.namviet.vtvtravel.config.Constants;
 import com.namviet.vtvtravel.databinding.F2ActivityTravelNewsBinding;
@@ -21,13 +30,19 @@ import com.namviet.vtvtravel.f2base.base.BaseFragment;
 import com.namviet.vtvtravel.view.fragment.f2travelnote.DetailNewsTravelFragment;
 import com.namviet.vtvtravel.view.fragment.f2travelnote.TravelNewsFragment;
 
+import java.util.Arrays;
+
 public class TravelNewsActivity extends BaseActivityNew<F2ActivityTravelNewsBinding> {
-    private InterstitialAd mInterstitialAd;
     public static final String DATA = "data";
     private boolean isTravelNews = true;
     private String detailLink;
     private int screenType;
+
+    private static final String AD_UNIT_ID = "ca-app-pub-3940256099942544/9214589741";
+    private FrameLayout adContainerView;
     private AdView adView;
+
+    private InterstitialAd mInterstitialAd;
 
 
     public class OpenType {
@@ -58,88 +73,45 @@ public class TravelNewsActivity extends BaseActivityNew<F2ActivityTravelNewsBind
 
     @Override
     public void doAfterOnCreate() {
-        MobileAds.initialize(this);
-        adView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().addTestDevice("323D3E715DE154B6B9929289FFA35B5E").build();
-        adView.loadAd(adRequest);
 
-        adView.setAdListener(new AdListener(){
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
-            public void onAdClosed() {
-                super.onAdClosed();
-            }
+            public void onInitializationComplete(InitializationStatus initializationStatus) {}
+        });
 
-            @Override
-            public void onAdFailedToLoad(int i) {
-                super.onAdFailedToLoad(i);
-            }
+        MobileAds.setRequestConfiguration(
+                new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("323D3E715DE154B6B9929289FFA35B5E")).build());
 
-            @Override
-            public void onAdLeftApplication() {
-                super.onAdLeftApplication();
-            }
+        adContainerView = findViewById(R.id.ad_view_container);
 
+        adContainerView.post(new Runnable() {
             @Override
-            public void onAdOpened() {
-                super.onAdOpened();
-            }
-
-            @Override
-            public void onAdLoaded() {
-                super.onAdLoaded();
-            }
-
-            @Override
-            public void onAdClicked() {
-                super.onAdClicked();
-            }
-
-            @Override
-            public void onAdImpression() {
-                super.onAdImpression();
+            public void run() {
+                loadBanner();
             }
         });
 
-        mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-        mInterstitialAd.loadAd(new AdRequest.Builder().addTestDevice("323D3E715DE154B6B9929289FFA35B5E").build());
 
-        mInterstitialAd.setAdListener(new AdListener(){
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
             @Override
-            public void onAdClosed() {
-                super.onAdClosed();
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                // The mInterstitialAd reference will be null until
+                // an ad is loaded.
+                mInterstitialAd = interstitialAd;
+
             }
 
             @Override
-            public void onAdFailedToLoad(int i) {
-                super.onAdFailedToLoad(i);
-            }
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                // Handle the error
 
-            @Override
-            public void onAdLeftApplication() {
-                super.onAdLeftApplication();
-            }
-
-            @Override
-            public void onAdOpened() {
-                super.onAdOpened();
-            }
-
-            @Override
-            public void onAdLoaded() {
-                super.onAdLoaded();
-            }
-
-            @Override
-            public void onAdClicked() {
-                super.onAdClicked();
-            }
-
-            @Override
-            public void onAdImpression() {
-                super.onAdImpression();
+                mInterstitialAd = null;
             }
         });
+
+
 
     }
 
@@ -173,13 +145,77 @@ public class TravelNewsActivity extends BaseActivityNew<F2ActivityTravelNewsBind
         activity.startActivity(intent);
     }
 
-    private AdSize getAdSize() {
-        return AdSize.FULL_BANNER;
-    }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        mInterstitialAd.show();
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(this);
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.");
+        }
     }
+
+    @Override
+    public void onPause() {
+        if (adView != null) {
+            adView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (adView != null) {
+            adView.resume();
+        }
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    private void loadBanner() {
+        // Create an ad request.
+        adView = new AdView(this);
+        adView.setAdUnitId(AD_UNIT_ID);
+        adContainerView.removeAllViews();
+        adContainerView.addView(adView);
+
+        AdSize adSize = getAdSize();
+        adView.setAdSize(adSize);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        // Start loading the ad in the background.
+        adView.loadAd(adRequest);
+    }
+
+    private AdSize getAdSize() {
+        // Determine the screen width (less decorations) to use for the ad width.
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+
+        float density = outMetrics.density;
+
+        float adWidthPixels = adContainerView.getWidth();
+
+        // If the ad hasn't been laid out, default to the full screen width.
+        if (adWidthPixels == 0) {
+            adWidthPixels = outMetrics.widthPixels;
+        }
+
+        int adWidth = (int) (adWidthPixels / density);
+
+        return AdSize.getCurrentOrientationBannerAdSizeWithWidth(this, adWidth);
+    }
+
 }
