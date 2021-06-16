808b5516694bb348c7ab235e65e81b90ac45e5d8
lamlv <lamlv@namviet-corp.com.vn>
2021-06-16 13:24:20 +0700

    notification firebase action

---
+++ app/src/main/java/com/namviet/vtvtravel/view/f3/notification/model/Data.java
@@ -0,0 +1,24 @@
+
+package com.namviet.vtvtravel.view.f3.notification.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Data implements Serializable {
+    @SerializedName("code")
+    @Expose
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+
+
+}
