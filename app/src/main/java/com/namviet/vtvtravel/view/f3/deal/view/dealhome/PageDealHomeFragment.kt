169f5f22ad3869b31d77f31d00fd085325a7f81c
Nguyen Duc Thanh <thanhnd.aptech@gmail.com>
2021-10-11 12:31:39 +0700

    Deal

--- app/src/main/java/com/namviet/vtvtravel/view/f3/deal/view/dealhome/PageDealHomeFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/f3/deal/view/dealhome/PageDealHomeFragment.kt
@@ -1,5 +1,8 @@
 package com.namviet.vtvtravel.view.f3.deal.view.dealhome
 
+import android.graphics.Color
+import androidx.viewpager.widget.ViewPager
+import com.daimajia.slider.library.Tricks.ViewPagerEx
 import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.databinding.FragmentPageDealHomeBinding
 import com.namviet.vtvtravel.f2base.base.BaseFragment
@@ -27,10 +30,66 @@ class PageDealHomeFragment : BaseFragment<FragmentPageDealHomeBinding?>() {
 
         vpContent.adapter = tabAdapter
         vpContent.offscreenPageLimit = 10
+        vpContent.currentItem = 1
 
     }
+
     override fun initData() {}
     override fun inject() {}
-    override fun setClickListener() {}
+    override fun setClickListener() {
+        btnMyGift.setOnClickListener {
+            vpContent.currentItem = 0
+            imgMyGift.setImageResource(R.drawable.ic_my_gift_selected)
+            tvMyGift.setTextColor(Color.parseColor("#F92418"))
+
+            imgDealSubscribe.setImageResource(R.drawable.ic_my_subcribe_unselected)
+            tvDealSubscribe.setTextColor(Color.parseColor("#707070"))
+        }
+        btnDealSubcribe.setOnClickListener {
+            vpContent.currentItem = 2
+
+            imgMyGift.setImageResource(R.drawable.ic_my_gift_unselected)
+            tvMyGift.setTextColor(Color.parseColor("#707070"))
+
+            imgDealSubscribe.setImageResource(R.drawable.ic_my_subcribe_selected)
+            tvDealSubscribe.setTextColor(Color.parseColor("#F92418"))
+        }
+        btnHome.setOnClickListener {
+            vpContent.currentItem = 1
+
+            imgMyGift.setImageResource(R.drawable.ic_my_gift_unselected)
+            tvMyGift.setTextColor(Color.parseColor("#707070"))
+
+            imgDealSubscribe.setImageResource(R.drawable.ic_my_subcribe_unselected)
+            tvDealSubscribe.setTextColor(Color.parseColor("#707070"))
+        }
+
+        vpContent.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> {
+                        btnMyGift.performClick()
+                    }
+                    1 -> {
+                        btnHome.performClick()
+                    }
+                    else -> {
+                        btnDealSubcribe.performClick()
+                    }
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
+    }
+
     override fun setObserver() {}
 }
