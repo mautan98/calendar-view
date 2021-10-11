99dd73c763fc5eda7874b1970b82c4b5389a5b37
Nguyen Duc Thanh <thanhnd.aptech@gmail.com>
2021-10-11 13:09:47 +0700

    Deal

--- app/src/main/java/com/namviet/vtvtravel/view/f3/deal/view/dealhome/DealHomeFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/f3/deal/view/dealhome/DealHomeFragment.kt
@@ -1,10 +1,14 @@
 package com.namviet.vtvtravel.view.f3.deal.view.dealhome
 
+import android.view.View
+import androidx.viewpager.widget.ViewPager
 import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.databinding.FragmentDealHomeBinding
 import com.namviet.vtvtravel.f2base.base.BaseFragment
 import com.namviet.vtvtravel.view.f3.deal.adapter.TabAdapter
+import kotlinx.android.synthetic.main.fragment_deal_home.*
 import kotlinx.android.synthetic.main.fragment_page_deal_home.*
+import kotlinx.android.synthetic.main.fragment_page_deal_home.vpContent
 
 class DealHomeFragment : BaseFragment<FragmentDealHomeBinding?>() {
     private var tabAdapter: TabAdapter? = null
@@ -25,6 +29,42 @@ class DealHomeFragment : BaseFragment<FragmentDealHomeBinding?>() {
     }
     override fun initData() {}
     override fun inject() {}
-    override fun setClickListener() {}
+    override fun setClickListener() {
+        layoutTab1.setOnClickListener {
+            vpContent.currentItem = 0
+            viewBorder1.visibility = View.VISIBLE
+            viewBorder2.visibility = View.GONE
+        }
+
+        layoutTab2.setOnClickListener {
+            vpContent.currentItem = 1
+            viewBorder1.visibility = View.GONE
+            viewBorder2.visibility = View.VISIBLE
+        }
+
+        vpContent.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> {
+                        layoutTab1.performClick()
+                    }
+                    1 -> {
+                        layoutTab2.performClick()
+                    }
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
+
+    }
     override fun setObserver() {}
 }
