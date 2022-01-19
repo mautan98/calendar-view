88c6661e34b30c1de8c85d228a6617e09f78cccf
Nguyen Duc Thanh <thanhnd.aptech@gmail.com>
2022-01-19 09:23:40 +0700

    Search UI

--- app/src/main/java/com/namviet/vtvtravel/ultils/F2Util.java
+++ app/src/main/java/com/namviet/vtvtravel/ultils/F2Util.java
@@ -34,7 +34,7 @@ public class F2Util {
 
     public static void startSendMessIntent(AppCompatActivity activity, String phone, String mess) {
         try {
-            Intent I = new Intent(Intent.ACTION_SENDTO);
+            Intent I = new Intent(Intent.ACTION_VIEW);
             I.setData(Uri.parse("smsto:"));
             I.setType("vnd.android-dir/mms-sms");
             I.putExtra("address", new String(phone));
