2a17393898947e5a95c35363fd2422a4faf7ab2d
Nguyen Duc Thanh <thanhnd.aptech@gmail.com>
2022-02-17 14:38:01 +0700

    Search UI

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2search/resultsearch/contentsort/DropDownStatusFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2search/resultsearch/contentsort/DropDownStatusFragment.kt
@@ -1,37 +1,22 @@
-package com.namviet.vtvtravel.view.fragment.f2search.resultsearch.contentsort;
+package com.namviet.vtvtravel.view.fragment.f2search.resultsearch.contentsort
 
-import com.namviet.vtvtravel.R;
-import com.namviet.vtvtravel.databinding.F3FragmentDropDownStatusBinding;
-import com.namviet.vtvtravel.f2base.base.BaseFragment;
+import com.namviet.vtvtravel.R
+import com.namviet.vtvtravel.databinding.F3FragmentDropDownStatusBinding
+import com.namviet.vtvtravel.f2base.base.BaseFragment
+import com.namviet.vtvtravel.model.f2search.Children
 
-public class DropDownStatusFragment extends BaseFragment<F3FragmentDropDownStatusBinding> {
-    @Override
-    public int getLayoutRes() {
-        return R.layout.f3_fragment_drop_down_status;
+class DropDownStatusFragment : BaseFragment<F3FragmentDropDownStatusBinding?>() {
+    override fun getLayoutRes(): Int {
+        return R.layout.f3_fragment_drop_down_status
     }
 
-    @Override
-    public void initView() {
+    override fun initView() {}
+    override fun initData() {}
+    override fun inject() {}
+    override fun setClickListener() {}
+    override fun setObserver() {}
 
-    }
-
-    @Override
-    public void initData() {
-
-    }
-
-    @Override
-    public void inject() {
-
-    }
-
-    @Override
-    public void setClickListener() {
-
-    }
-
-    @Override
-    public void setObserver() {
-
+    public interface Listener{
+        fun onApply()
     }
 }
