4620d1edc3c27c4f5263696895226832aebba7b9
TheUniverse779 <thanhnd7779@gmail.com>
2022-02-22 09:29:01 +0700

    Search news UI

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2search/ChooseRegionFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2search/ChooseRegionFragment.kt
@@ -1,47 +1,102 @@
-package com.namviet.vtvtravel.view.fragment.f2search;
+package com.namviet.vtvtravel.view.fragment.f2search
 
-import android.view.View;
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.fragment.app.FragmentTransaction
+import com.namviet.vtvtravel.R
+import com.namviet.vtvtravel.adapter.f2biglocation.SearchAllLocationAdapter
+import com.namviet.vtvtravel.databinding.F3LayoutSearchDestinationBinding
+import com.namviet.vtvtravel.f2base.base.BaseFragment
+import com.namviet.vtvtravel.model.travelnews.Location
+import com.namviet.vtvtravel.viewmodel.f2biglocation.SearchBigLocationViewModel
+import kotlinx.android.synthetic.main.f3_fragment_search_region_main.*
+import kotlinx.android.synthetic.main.f3_layout_search_destination.*
 
-import androidx.fragment.app.FragmentTransaction;
+class ChooseRegionFragment : BaseFragment<F3LayoutSearchDestinationBinding?>() {
+    private var viewModel: SearchBigLocationViewModel? = null
+    private var locationsMain: ArrayList<Location>? = null
+    private val locations: ArrayList<Location> = ArrayList()
+    private var chooseRegion: ChooseRegion? = null
+    private var searchAllLocationAdapter: SearchAllLocationAdapter? = null
+    private var mContext : Context?  =null
 
-import com.namviet.vtvtravel.R;
-import com.namviet.vtvtravel.databinding.F3LayoutSearchDestinationBinding;
-import com.namviet.vtvtravel.f2base.base.BaseFragment;
-
-public class ChooseRegionFragment extends BaseFragment<F3LayoutSearchDestinationBinding> {
-    @Override
-    public int getLayoutRes() {
-        return R.layout.f3_layout_search_destination;
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+    override fun getLayoutRes(): Int {
+        return R.layout.f3_layout_search_destination
     }
 
-    @Override
-    public void initView() {
+    override fun initView() {}
+    override fun initData() {
+//        viewModel = SearchBigLocationViewModel()
+//        viewModel!!.addObserver(this)
+        locations.addAll(locationsMain!!)
+        searchAllLocationAdapter = SearchAllLocationAdapter(
+            mContext,
+            locations,
+            SearchAllLocationAdapter.ClickItem { location ->
+                mActivity.onBackPressed()
+                chooseRegion?.clickRegion(location)
 
+
+            })
+        rclCity.adapter = searchAllLocationAdapter
+        handleSearch()
+    }
+    override fun inject() {}
+    override fun setClickListener() {
+        binding!!.btnCloseFilter.setOnClickListener {
+            assert(fragmentManager != null)
+            fragmentManager!!.beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+                .remove(this@ChooseRegionFragment).commit()
+        }
     }
 
-    @Override
-    public void initData() {
-
+    public fun setData(locationsMain: ArrayList<Location>?, chooseRegion: ChooseRegion?) {
+        this.locationsMain = locationsMain
+        this.chooseRegion = chooseRegion
     }
 
-    @Override
-    public void inject() {
-
+    public interface ChooseRegion {
+        fun clickRegion(location: Location?)
     }
 
-    @Override
-    public void setClickListener() {
-        getBinding().btnCloseFilter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                assert getFragmentManager() != null;
-                getFragmentManager().beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE).remove(ChooseRegionFragment.this).commit();
-            }
-        });
+    override fun setObserver() {}
+
+    @SuppressLint("CheckResult")
+    private fun handleSearch() {
+//        RxTextView.afterTextChangeEvents(binding!!.edtSearch)
+//            .skipInitialValue()
+//            .debounce(790, TimeUnit.MILLISECONDS)
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe {
+//                try {
+//                    binding!!.rclLocation.visibility = View.VISIBLE
+//                    locations.clear()
+//                    for (i in locationsMain!!.indices) {
+//                        if (F2Util.removeAccent(locationsMain!![i].name.toLowerCase()).contains(
+//                                F2Util.removeAccent(
+//                                    binding!!.edtSearch.text.toString().toLowerCase()
+//                                )
+//                            )
+//                        ) {
+//                            locations.add(locationsMain!![i])
+//                        }
+//                    }
+//                    searchAllLocationAdapter!!.notifyDataSetChanged()
+//                } catch (e: Exception) {
+//                }
+//            }
     }
 
-    @Override
-    public void setObserver() {
-
+    override fun onDestroy() {
+        super.onDestroy()
+//        try {
+//            KeyboardUtils.hideKeyboard(mActivity, binding!!.edtSearch)
+//        } catch (e: Exception) {
+//        }
     }
 }
--- app/src/main/java/com/namviet/vtvtravel/view/fragment/imagepart/HighLightestImagesFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/imagepart/HighLightestImagesFragment.kt
@@ -1,112 +1,149 @@
-package com.namviet.vtvtravel.view.fragment.f2video
+package com.namviet.vtvtravel.view.fragment.imagepart
 
-import android.graphics.Color
-import android.os.Bundle
-import android.os.Handler
-import android.view.LayoutInflater
+import android.annotation.SuppressLint
 import android.view.View
-import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.TextView
 import androidx.core.view.GravityCompat
-import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.tabs.TabLayout
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.adapter.f2video.SortVideoAdapter
-import com.namviet.vtvtravel.adapter.vtvtabstyle.VTVTabStyleAdapter
-import com.namviet.vtvtravel.databinding.F2FragmentVideoBinding
+import com.namviet.vtvtravel.adapter.imagepart.HighLightestImagesAdapter
+import com.namviet.vtvtravel.app.MyApplication
+import com.namviet.vtvtravel.databinding.F2FragmentHighLightestImagesBinding
+import com.namviet.vtvtravel.f2base.base.BaseFragment
 import com.namviet.vtvtravel.f2errorresponse.ErrorResponse
 import com.namviet.vtvtravel.model.f2search.Children
 import com.namviet.vtvtravel.model.f2search.SortAndFilter
 import com.namviet.vtvtravel.model.travelnews.Location
 import com.namviet.vtvtravel.response.f2biglocation.AllLocationResponse
-import com.namviet.vtvtravel.response.f2biglocation.LocationResponse
-import com.namviet.vtvtravel.response.f2video.VideoResponse
+import com.namviet.vtvtravel.response.imagepart.ItemImagePartResponse
 import com.namviet.vtvtravel.tracking.TrackingAnalytic
 import com.namviet.vtvtravel.ultils.F2Util
-import com.namviet.vtvtravel.view.f2.f2oldbase.SearchActivity
-import com.namviet.vtvtravel.view.fragment.MainFragment
+import com.namviet.vtvtravel.view.f2.LoginAndRegisterActivityNew
 import com.namviet.vtvtravel.view.fragment.f2search.ChooseRegionFragment
-import com.namviet.vtvtravel.view.fragment.f2search.resultsearch.SlideMenuSearchFragment
-import com.namviet.vtvtravel.view.fragment.f2search.resultsearch.contentsort.DropDownLocationFragment
 import com.namviet.vtvtravel.view.fragment.f2search.resultsearch.contentsort.SortFollowFragment
+import com.namviet.vtvtravel.view.fragment.f2video.DropDownLocationInVideoFragment
 import com.namviet.vtvtravel.viewmodel.f2biglocation.SearchBigLocationViewModel
-import com.namviet.vtvtravel.viewmodel.f2video.VideoViewModel
-import kotlinx.android.synthetic.main.f2_fragment_search.*
+import com.namviet.vtvtravel.viewmodel.imagepart.ImagePartViewModel
 import java.util.*
 
-class VideoFragment : MainFragment(), Observer {
-    private var binding: F2FragmentVideoBinding? = null
-    private var viewModel: VideoViewModel? = null
-    private var videoResponse: VideoResponse? = null
+class HighLightestImagesFragment : BaseFragment<F2FragmentHighLightestImagesBinding?>, Observer {
+    private var highLightestImagesAdapter: HighLightestImagesAdapter? = null
+    private val items: MutableList<ItemImagePartResponse.Data.Item> = ArrayList()
+    private var viewModel: ImagePartViewModel? = null
+    private var loadMoreLink: String? = null
+
+
     private var sortVideoAdapter: SortVideoAdapter? = null
     private var sortAndFilter: SortAndFilter? = null
     private var locationsMain: ArrayList<Location> = ArrayList()
     private val locations: ArrayList<Location>? = ArrayList()
     private var locationViewModel: SearchBigLocationViewModel? = null
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @SuppressLint("ValidFragment")
+    constructor(idGallery: String?) {
+        this.idGallery = idGallery
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.f2_fragment_video, container, false)
-        return binding?.root
+    constructor() {}
+
+    private var idGallery: String? = null
+    override fun getLayoutRes(): Int {
+        return R.layout.f2_fragment_high_lightest_images
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        try {
-            TrackingAnalytic.postEvent(
-                TrackingAnalytic.SCREEN_VIEW,
-                TrackingAnalytic.getDefault(
-                    TrackingAnalytic.ScreenCode.VIDEOS,
-                    TrackingAnalytic.ScreenTitle.VIDEOS
-                ).setScreen_class(this.javaClass.name)
-            )
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        initViews(view)
-    }
-
-    override fun initViews(v: View) {
-        super.initViews(v)
-        binding!!.vpContent.offscreenPageLimit = 10
-        viewModel = VideoViewModel()
-        binding!!.videoViewModel = viewModel
-        viewModel!!.addObserver(this)
-
+    override fun initView() {
         locationViewModel = SearchBigLocationViewModel()
         locationViewModel?.addObserver(this)
         locationViewModel?.getAllLocation()
 
-        Handler().postDelayed({ viewModel!!.getCategoryVideo() }, 500)
-        binding!!.btnSearch.setOnClickListener { SearchActivity.startScreen(mActivity) }
         filterData
         initSlideMenu()
     }
+    override fun initData() {
+        viewModel = ImagePartViewModel()
+        binding!!.imagePartViewModel = viewModel
+        viewModel!!.addObserver(this)
+        viewModel!!.getItemGallery(idGallery)
 
-    private var mainAdapter: VTVTabStyleAdapter? = null
-    override fun update(observable: Observable, o: Any) {
-        Handler().postDelayed({
-            try {
-                binding!!.shimmerViewContainer.stopShimmer()
-                binding!!.shimmerViewContainer.visibility = View.GONE
-                binding!!.vpContent.visibility = View.VISIBLE
-            } catch (e: Exception) {
-                e.printStackTrace()
+//        itemImagePartResponse = new Gson().fromJson(json, ItemImagePartResponse.class);
+        highLightestImagesAdapter = HighLightestImagesAdapter(
+            items,
+            mActivity,
+            object : HighLightestImagesAdapter.ClickItem {
+                override fun onClickItem(position: Int) {}
+                override fun likeEvent(position: Int) {
+                    try {
+                        val account = MyApplication.getInstance().account
+                        val video = items[position]
+                        if (null != account && account.isLogin) {
+                            viewModel!!.likeEvent(video.id, video.content_type)
+                            try {
+                                TrackingAnalytic.postEvent(
+                                    TrackingAnalytic.LIKE,
+                                    TrackingAnalytic.getDefault(
+                                        TrackingAnalytic.ScreenCode.HIGH_LIGHTEST_IMAGE,
+                                        TrackingAnalytic.ScreenTitle.HIGH_LIGHTEST_IMAGE
+                                    )
+                                        .setContent_type(video.content_type)
+                                        .setContent_id(video.id)
+                                        .setScreen_class(this.javaClass.name)
+                                )
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+                            if (video.isLiked) {
+                                video.isLiked = false
+                                if (null != video.likeCount) {
+                                    val likeCount = (video.likeCount.toInt() - 1).toString() + ""
+                                    video.likeCount = likeCount
+                                }
+                            } else {
+                                video.isLiked = true
+                                if (null != video.likeCount) {
+                                    val likeCount = (video.likeCount.toInt() + 1).toString() + ""
+                                    video.likeCount = likeCount
+                                }
+                            }
+                            highLightestImagesAdapter!!.notifyItemChanged(position)
+                        } else {
+                            LoginAndRegisterActivityNew.startScreen(mActivity, 0, false)
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            })
+        binding!!.rclContent.adapter = highLightestImagesAdapter
+        binding!!.rclContent.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1)) {
+                    viewModel!!.getGalleryByUrl(loadMoreLink, true)
+                    loadMoreLink = ""
+                }
             }
-        }, 1000)
+        })
+    }
+
+    override fun inject() {}
+    override fun setClickListener() {
+        binding!!.btnBack.setOnClickListener { mActivity.onBackPressed() }
+    }
+
+    override fun setObserver() {}
+    override fun update(observable: Observable, o: Any) {
+        hideLoading()
+        try {
+            binding!!.shimmerViewContainer.stopShimmer()
+            binding!!.shimmerViewContainer.visibility = View.GONE
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
         if (observable is SearchBigLocationViewModel && null != o) {
             when (o) {
                 is AllLocationResponse -> {
@@ -123,63 +160,35 @@ class VideoFragment : MainFragment(), Observer {
 //                    }
 //                }
             }
-        } else if (observable is VideoViewModel && null != o) {
-            if (o is VideoResponse) {
-                videoResponse = o
-                mainAdapter = VTVTabStyleAdapter(childFragmentManager)
-                for (i in videoResponse!!.data.indices) {
-                    val subVideoFragment = SubVideoFragment()
-                    subVideoFragment.setContentLink(videoResponse!!.data[i].link)
-                    mainAdapter!!.addFragment(subVideoFragment, "")
+        } else if (observable is ImagePartViewModel && null != o) {
+            if (o is ItemImagePartResponse) {
+                val response = o
+                loadMoreLink = response.data.more_link
+                if (response.isLoadMore) {
+                    items.addAll(response.data.items)
+                } else {
+                    items.clear()
+                    items.addAll(response.data.items)
                 }
-                binding!!.vpContent.adapter = mainAdapter
-                binding!!.tabLayout.setupWithViewPager(binding!!.vpContent)
-                for (i in videoResponse!!.data.indices) {
-                    val tabHome = LayoutInflater.from(mActivity)
-                        .inflate(R.layout.f2_custom_tab_vtv_style, null)
-                    val tvHome = tabHome.findViewById<TextView>(R.id.tvTitle)
-                    tvHome.text = videoResponse!!.data[i].name
-                    if (i == 0) {
-                        tvHome.setTextColor(Color.parseColor("#00918D"))
-                    } else {
-                        tvHome.setTextColor(Color.parseColor("#101010"))
-                    }
-                    val view = tabHome.findViewById<View>(R.id.indicator)
-                    if (i == 0) {
-                        view.visibility = View.VISIBLE
-                    } else {
-                        view.visibility = View.INVISIBLE
-                    }
-                    binding!!.tabLayout.getTabAt(i)!!.customView = tabHome
-                }
-                binding!!.tabLayout.addOnTabSelectedListener(OnTabSelectedListener)
-            }
-        } else if (o is ErrorResponse) {
-            val responseError = o
-            try {
+                highLightestImagesAdapter!!.notifyDataSetChanged()
+            } else if (o is ErrorResponse) {
+                val responseError = o
+                try {
 //                    ((LoginAndRegisterActivityNew) mActivity).showWarning(responseError.getMessage());
-            } catch (e: Exception) {
+                } catch (e: Exception) {
+                }
             }
         }
     }
 
-    private val OnTabSelectedListener: TabLayout.OnTabSelectedListener =
-        object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab) {
-                val c = tab.position
-                mainAdapter!!.SetOnSelectView(binding!!.tabLayout, c)
-            }
+    override fun setScreenTitle() {
+        super.setScreenTitle()
+        setDataScreen(
+            TrackingAnalytic.ScreenCode.HIGH_LIGHTEST_IMAGE,
+            TrackingAnalytic.ScreenTitle.HIGH_LIGHTEST_IMAGE
+        )
+    }
 
-            override fun onTabUnselected(tab: TabLayout.Tab) {
-                val c = tab.position
-                mainAdapter!!.SetUnSelectView(binding!!.tabLayout, c)
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab) {}
-        }
-
-    //                                hideMenuAnim()
-//                                sortAdapter?.notifyDataSetChanged()
     private val filterData: Unit
         private get() {
             sortAndFilter = Gson().fromJson(
@@ -256,10 +265,8 @@ class VideoFragment : MainFragment(), Observer {
     }
 
     private fun createMenuFragment(){
-        var chooseRegionFragment = ChooseRegionFragment()
-        chooseRegionFragment.setData(locations, null)
         fragmentManager?.beginTransaction()
-            ?.add(R.id.chooseRegionFrame, chooseRegionFragment)
+            ?.add(R.id.chooseRegionFrame, ChooseRegionFragment())
             ?.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)?.addToBackStack(null)!!.commit()
     }
 
