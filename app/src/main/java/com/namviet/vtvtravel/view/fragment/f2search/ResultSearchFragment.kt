dfaab3573a1e011a1d6b6973afffc0e71f440a67
Nguyen Duc Thanh <thanhnd.aptech@gmail.com>
2022-03-30 10:17:46 +0700

    Search

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2search/ResultSearchFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2search/ResultSearchFragment.kt
@@ -490,7 +490,13 @@ class ResultSearchFragment : BaseFragment<F2FragmentResultSearchBinding>, Observ
                 } else {
                     type[0] = "video"
                 }
-                sortAdapter?.notifyDataSetChanged()
+                try {
+                    sortAdapter?.notifyDataSetChanged()
+                    if (layoutExpand.visibility == View.VISIBLE) {
+                        hideMenuAnim()
+                    }
+                } catch (e: Exception) {
+                }
             }
 
             override fun onPageScrollStateChanged(state: Int) {
