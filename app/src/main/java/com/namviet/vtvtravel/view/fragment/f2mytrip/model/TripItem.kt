3b072840b2b601914b44e6f911e0fdfb3c8d3b9c
hainv <hainv@namviet-corp.com.vn>
2022-05-26 13:46:51 +0700

    add trip item element

---
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/model/TripItem.kt
@@ -0,0 +1,45 @@
+package com.namviet.vtvtravel.view.fragment.f2mytrip.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class TripItem(
+
+	@field:SerializedName("creator")
+	val creator: Creator? = null,
+
+	@field:SerializedName("userList")
+	val userList: List<UserListItem?>? = null,
+
+	@field:SerializedName("checkRights")
+	val checkRights: Any? = null,
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("bannerUrl")
+	val bannerUrl: String? = null,
+
+	@field:SerializedName("description")
+	val description: String? = null,
+
+	@field:SerializedName("schedulePlaceByDays")
+	val schedulePlaceByDays: Any? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("estimatedCost")
+	val estimatedCost: Int? = null,
+
+	@field:SerializedName("endAt")
+	val endAt: Long? = null,
+
+	@field:SerializedName("startAt")
+	val startAt: Long? = null,
+
+	@field:SerializedName("numberPeople")
+	val numberPeople: Int? = null
+) : Parcelable
