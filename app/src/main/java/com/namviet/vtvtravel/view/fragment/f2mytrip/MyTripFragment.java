3ef953c72d7657fce5550b32dc0a4b47db4e8d9c
hainv <hainv@namviet-corp.com.vn>
2022-05-26 13:16:39 +0700

    remove webview in java code

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/MyTripFragment.java
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/MyTripFragment.java
@@ -33,59 +33,14 @@ public class MyTripFragment extends BaseFragment<F2FragmentMyTripBinding> {
 
     @Override
     public void initView() {
-        getBinding().webView.getSettings().setJavaScriptEnabled(true);
 
-        getBinding().webView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-                if (progress == 100) { //...page is fully loaded.
-                    try {
-                        getBinding().shimmerViewContainer.stopShimmer();
-                        getBinding().layoutLoading.setVisibility(View.GONE);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
 
-        getBinding().webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if(url.toUpperCase().equals("APP://EXIT")){
-                    mActivity.onBackPressed();
-                } else if (url.toUpperCase().startsWith("APP://DETAIL")) {
-                    try {
-                        String link = new UrlQuerySanitizer(url).getValue("link");
-                        SmallLocationActivity.startScreenDetail(mActivity, SmallLocationActivity.OpenType.DETAIL, link);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }else if(url.toUpperCase().startsWith("APP://MAP")){
-                    try {
-                        String link = new UrlQuerySanitizer(url).getValue("link");
-                        DisplayMarkerForMapActivity.startScreen(mActivity, link);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }else if(url.toUpperCase().equals("APP://CALL")){
-                    try {
-                        String phone = "1039";
-                        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
-                        mActivity.startActivity(intent);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }else {
-                    view.loadUrl(url);
-                }
-                return true;
-            }
-        });
+
     }
 
     @Override
     public void initData() {
-        getBinding().webView.loadUrl(genLink(), extraHeaders);
+
     }
 
     @Override
@@ -103,28 +58,4 @@ public class MyTripFragment extends BaseFragment<F2FragmentMyTripBinding> {
 
     }
 
-    private String genLink() {
-        try {
-            Double lat = null;
-            Double lng = null;
-            if (null != MyApplication.getInstance().getMyLocation()
-                    && MyApplication.getInstance().getMyLocation().getLat() != MyApplication.getInstance().getMyLocation().getLog()) {
-                lat = MyApplication.getInstance().getMyLocation().getLat();
-                lng = MyApplication.getInstance().getMyLocation().getLog();
-            }
-            Account account = MyApplication.getInstance().getAccount();
-            if (null != account && account.isLogin() && lat != null && lng != null) {
-                extraHeaders.put("token",account.getToken());
-                return link  + "?lat=" + lat + "&long=" + lng +"&channel=android&langCode=vi"+"&backLink="+"true";
-            } else if (null != account && account.isLogin()) {
-                extraHeaders.put("token",account.getToken());
-                return link  +"?channel=android&langCode=vi"+"&backLink="+"true";
-            }else {
-                return link +"?channel=android&langCode=vi"+"&backLink="+"true";
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return link +"?channel=android&langCode=vi"+"&backLink="+"true";
-        }
-    }
 }
