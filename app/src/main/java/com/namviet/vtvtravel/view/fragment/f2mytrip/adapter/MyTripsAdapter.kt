919e90dd33704a1113947c30156596e9aea53335
hainv <hainv@namviet-corp.com.vn>
2022-05-26 13:48:09 +0700

    add empty mytrips adapter

---
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/adapter/MyTripsAdapter.kt
@@ -0,0 +1,35 @@
+package com.namviet.vtvtravel.view.fragment.f2mytrip.adapter
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.namviet.vtvtravel.databinding.LayoutItemMyTripsBinding
+import com.namviet.vtvtravel.view.fragment.f2mytrip.model.MyTripsResponse
+import com.namviet.vtvtravel.view.fragment.f2mytrip.model.TripItem
+
+class MyTripsAdapter : RecyclerView.Adapter<MyTripsAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
+    private var listTripItem: MutableList<TripItem> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return listTripItem.size
+    }
+
+    inner class ViewHolder(var binding: LayoutItemMyTripsBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(position: Int) {
+            MyTripsResponse().data?.listTrip
+        }
+
+    }
+
+}
