61ff1774c79e1223febddab070b183e08245a337
hainv <hainv@namviet-corp.com.vn>
2022-05-27 11:33:10 +0700

    format estimate cost, time

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/adapter/MyTripsAdapter.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/adapter/MyTripsAdapter.kt
@@ -1,12 +1,15 @@
 package com.namviet.vtvtravel.view.fragment.f2mytrip.adapter
 
 import android.content.Context
+import android.os.Build
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.namviet.vtvtravel.R
+import com.namviet.vtvtravel.Utils
 import com.namviet.vtvtravel.databinding.LayoutItemMyTripsBinding
 import com.namviet.vtvtravel.view.fragment.f2mytrip.model.TripItem
 import java.util.*
@@ -43,13 +46,17 @@ class MyTripsAdapter(var context: Context) : RecyclerView.Adapter<MyTripsAdapter
             binding.tvCreatorName.text = tripItem.creator?.fullname
             binding.tvTripsName.text = tripItem.name
             binding.tvTripsDesc.text = tripItem.description
-            binding.tvEstimateCost.text = tripItem.estimatedCost.toString()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                binding.tvEstimateCost.text = Html.fromHtml(Utils.convertPriceTrips(tripItem.estimatedCost),Html.FROM_HTML_MODE_COMPACT)
+            } else {
+                binding.tvEstimateCost.text = Html.fromHtml(Utils.convertPriceTrips(tripItem.estimatedCost))
+            }
             val diff = tripItem.startAt?.let { tripItem.endAt?.minus(it) }
             if (tripItem.startAt == tripItem.endAt){
                 binding.tvDaysTrip.text = "1 Ngày"
             } else {
                 binding.tvDaysTrip.text =
-                    "${diff?.let { TimeUnit.DAYS.convert(it, TimeUnit.MILLISECONDS) }} Ngày"
+                    "${diff?.let { TimeUnit.DAYS.convert(it, TimeUnit.MILLISECONDS) + 1}} Ngày"
             }
             binding.tvAmountPeople.text = tripItem.numberPeople.toString()
             binding.tvStartDate.text = formatTimestamp(tripItem.startAt)
