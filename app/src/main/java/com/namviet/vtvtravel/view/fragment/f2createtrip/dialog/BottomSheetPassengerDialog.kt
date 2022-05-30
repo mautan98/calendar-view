1525072ce81b17bf234353942a4ab98786e28365
hainv <hainv@namviet-corp.com.vn>
2022-05-30 08:38:28 +0700

    implement event onclick in bottom sheet

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2createtrip/dialog/BottomSheetPassengerDialog.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2createtrip/dialog/BottomSheetPassengerDialog.kt
@@ -2,30 +2,55 @@ package com.namviet.vtvtravel.view.fragment.f2createtrip.dialog
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.databinding.LayoutChooseNumOfPassengerBinding
 import com.namviet.vtvtravel.view.fragment.f2offline.BaseDialogBottom
 
-class BottomSheetPassengerDialog(context: Context) : BaseDialogBottom() {
+class BottomSheetPassengerDialog(context: Context) : BottomSheetDialogFragment() {
 
     private lateinit var binding:LayoutChooseNumOfPassengerBinding
     private var applyListener: ApplyPassengerListener? =null
 
-    override fun getLayoutResource(): Int {
-       return R.layout.layout_choose_num_of_passenger
+    fun setApplyListener(applyListener: ApplyPassengerListener){
+        this.applyListener = applyListener
     }
 
-    override fun init(saveInstanceState: Bundle?, view: View?) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.layout_choose_num_of_passenger,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initClickListener()
+    }
+
+    private fun initView(){
 
     }
 
-    override fun setUp(view: View?) {
-        TODO("Not yet implemented")
+    private fun initClickListener(){
+        val numAdult = binding.tvNumberPassenger.text.toString().toInt()
+        val numChildren = binding.tvNumberChildren.text.toString().toInt()
+        val numBaby = binding.tvNumberBaby.text.toString().toInt()
+        binding.btnApply.setOnClickListener{
+            dismiss()
+            applyListener?.onApplyClick(numAdult,numChildren,numBaby)
+        }
     }
 
     interface ApplyPassengerListener{
-        fun onApplyClick()
+        fun onApplyClick(numAdult:Int,numChildren:Int,numBaby:Int)
     }
 
 }
