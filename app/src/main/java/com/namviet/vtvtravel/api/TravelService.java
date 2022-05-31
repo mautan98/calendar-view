d9d64e6c53c2e6a2121ff7efb7e97c28e0dc6314
hainv <hainv@namviet-corp.com.vn>
2022-05-31 09:47:41 +0700

    modify observable type

--- app/src/main/java/com/namviet/vtvtravel/api/TravelService.java
+++ app/src/main/java/com/namviet/vtvtravel/api/TravelService.java
@@ -117,6 +117,8 @@ import com.namviet.vtvtravel.view.f3.deal.model.dealcampaign.DealCampaignDetail;
 import com.namviet.vtvtravel.view.f3.deal.model.dealfollow.DealFollowResponse;
 import com.namviet.vtvtravel.view.f3.deal.model.mygift.MyGiftResponse;
 import com.namviet.vtvtravel.view.fragment.f2mytrip.model.MyTripsResponse;
+import com.namviet.vtvtravel.view.fragment.f2mytrip.model.createschedule.CreateScheduleResponse;
+import com.namviet.vtvtravel.view.fragment.f2mytrip.model.createschedule.DataCreateTrips;
 import com.namviet.vtvtravel.view.fragment.f2service.GetInfoResponse;
 import com.namviet.vtvtravel.view.fragment.f2service.ResentOtpServiceResponse;
 import com.namviet.vtvtravel.view.fragment.f2service.ServiceOtpResponse;
@@ -767,7 +769,7 @@ public interface TravelService {
     Observable<MyTripsResponse> getAllTripsSchedule();
 
     @POST(WSConfig.API_TRIPS_SCHEDULE+WSConfig.Api.CREATE_SCHEDULE)
-    Observable<MyTripsResponse> createTrip(@Body RequestBody body);
+    Observable<CreateScheduleResponse> createTrip(@Body DataCreateTrips body);
 
 
 }
