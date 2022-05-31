d531fa9258cce651231bcea78aabfacba31e75c0
hainv <hainv@namviet-corp.com.vn>
2022-05-31 11:35:02 +0700

    add body request create schedule trip

---
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/model/createschedule/BodyCreateTrip.kt
@@ -0,0 +1,34 @@
+package com.namviet.vtvtravel.view.fragment.f2mytrip.model.createschedule
+
+import com.google.gson.annotations.SerializedName
+
+class BodyCreateTrip {
+
+    @field:SerializedName("name")
+    var name:String?= null
+
+    @field:SerializedName("description")
+    var description:String?= null
+
+    @field:SerializedName("placeCheckInId")
+    var placeCheckInId: String? = null
+
+    @field:SerializedName("placeCheckOutId")
+    var placeCheckOutId: String? = null
+
+    @field:SerializedName("startAt")
+    var startAt: String? = null
+
+    @field:SerializedName("endAt")
+    var endAt: String? = null
+
+    @field:SerializedName("adults")
+    var adults: Int? = null
+
+    @field:SerializedName("children")
+    var children: Int? = null
+
+    @field:SerializedName("infant")
+    var infant: Int? = null
+
+}
