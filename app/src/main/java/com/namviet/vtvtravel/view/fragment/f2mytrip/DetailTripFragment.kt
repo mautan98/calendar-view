84ec46eb8940afadfd2ca6e6d30e26ce8569121f
hainv <hainv@namviet-corp.com.vn>
2022-06-01 16:40:22 +0700

    add fragment detail trip

---
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/DetailTripFragment.kt
@@ -0,0 +1,31 @@
+package com.namviet.vtvtravel.view.fragment.f2mytrip
+
+import com.namviet.vtvtravel.databinding.FragmentDetailTripBinding
+import com.namviet.vtvtravel.f2base.base.BaseFragment
+
+class DetailTripFragment: BaseFragment<FragmentDetailTripBinding>() {
+
+    override fun getLayoutRes(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun inject() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setClickListener() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setObserver() {
+        TODO("Not yet implemented")
+    }
+}
