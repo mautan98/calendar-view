25a7195995ce32a899a2adb6042b108e9418a337
hainv <hainv@namviet-corp.com.vn>
2022-06-02 10:00:37 +0700

    fill information text from trip item

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/DetailTripFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/DetailTripFragment.kt
@@ -1,7 +1,10 @@
 package com.namviet.vtvtravel.view.fragment.f2mytrip
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import com.namviet.vtvtravel.R
+import com.namviet.vtvtravel.Utils
 import com.namviet.vtvtravel.databinding.FragmentDetailTripBinding
 import com.namviet.vtvtravel.f2base.base.BaseFragment
 import com.namviet.vtvtravel.view.fragment.f2mytrip.model.TripItem
@@ -37,6 +40,15 @@ class DetailTripFragment: BaseFragment<FragmentDetailTripBinding>() {
 
         binding.tvDetailTripName.text = tripItem?.name
         binding.tvDetailTripDesc.text = tripItem?.description
+        val startDate = Utils.formatTimestampTrips(tripItem?.startAt)
+        val endDate = Utils.formatTimestampTrips(tripItem?.endAt)
+        binding.tvDetailTimeTrips.text = "($startDate - $endDate, ${tripItem?.numberPeople})"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            binding.tvEstimateCost.text = Html.fromHtml(Utils.convertPriceTrips(tripItem?.estimatedCost),
+                Html.FROM_HTML_MODE_COMPACT)
+        } else {
+            binding.tvEstimateCost.text = Html.fromHtml(Utils.convertPriceTrips(tripItem?.estimatedCost))
+        }
     }
 
     override fun inject() {
