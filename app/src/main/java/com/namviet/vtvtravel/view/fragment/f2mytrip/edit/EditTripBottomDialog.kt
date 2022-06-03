3b9588d4d96affc15e3f65fe1ce4874dc86eb623
hainv <hainv@namviet-corp.com.vn>
2022-06-03 15:38:45 +0700

    add transparent and newinstance method

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/edit/EditTripBottomDialog.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/edit/EditTripBottomDialog.kt
@@ -1,5 +1,6 @@
 package com.namviet.vtvtravel.view.fragment.f2mytrip.edit
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +9,28 @@ import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.databinding.LayoutBottomDialogEditTripBinding
+import com.namviet.vtvtravel.view.fragment.f2mytrip.DetailTripFragment
+import com.namviet.vtvtravel.view.fragment.f2mytrip.model.TripItem
+import java.util.*
 
 class EditTripBottomDialog : BottomSheetDialogFragment() {
 
+    companion object {
+        fun newInstance(tripItem: TripItem):EditTripBottomDialog {
+            val args = Bundle()
+            args.putParcelable(DetailTripFragment.KEY_TRIP_ITEM,tripItem)
+            val fragment = EditTripBottomDialog()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
     private lateinit var binding: LayoutBottomDialogEditTripBinding
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (Objects.requireNonNull(view)!!.parent as View).setBackgroundColor(Color.TRANSPARENT)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
