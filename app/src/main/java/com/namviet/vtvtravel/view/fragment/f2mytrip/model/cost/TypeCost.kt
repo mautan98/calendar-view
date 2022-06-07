0a477b7706f3aa28d582abeddd4e18a7a8de04dd
hainv <hainv@namviet-corp.com.vn>
2022-06-07 14:40:27 +0700

    add object type cost

---
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/model/cost/TypeCost.kt
@@ -0,0 +1,36 @@
+package com.namviet.vtvtravel.view.fragment.f2mytrip.model.cost
+
+class TypeCost {
+     var resourceImage: Int = 0
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    constructor(resourceImage: Int, costName: String?) {
+        this.resourceImage = resourceImage
+        this.costName = costName
+    }
+
+    var costName: String? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+     var pricePP: Float? = null
+         get() = field
+         set(value) {
+             field = value
+         }
+    var amount: Int? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+    var totalPrice: Float? = null
+        get() = field
+        set(value) {
+            field = value
+        }
+
+}
