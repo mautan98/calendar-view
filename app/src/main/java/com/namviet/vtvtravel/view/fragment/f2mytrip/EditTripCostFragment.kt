7793f366b3d3f32396b3d4a534de883d184f5df4
hainv <hainv@namviet-corp.com.vn>
2022-06-08 15:12:41 +0700

    implement get cost api viewmodel

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/EditTripCostFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/EditTripCostFragment.kt
@@ -5,14 +5,18 @@ import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.databinding.FragmentAddEstimateCostBinding
 import com.namviet.vtvtravel.f2base.base.BaseFragment
 import com.namviet.vtvtravel.view.fragment.f2mytrip.adapter.CostTypesAdapter
+import com.namviet.vtvtravel.view.fragment.f2mytrip.model.cost.CostResponse
 import com.namviet.vtvtravel.view.fragment.f2mytrip.model.cost.TypeCost
+import com.namviet.vtvtravel.view.fragment.f2mytrip.viewmodel.MyTripsViewModel
+import java.util.*
 
-class EditTripCostFragment: BaseFragment<FragmentAddEstimateCostBinding>() {
+class EditTripCostFragment: BaseFragment<FragmentAddEstimateCostBinding>(), Observer {
 
     companion object {
-
-        fun newInstance():EditTripCostFragment {
+        const val KEY_SCHEDULE_ID = "key_schedule_id"
+        fun newInstance(scheduleId:String?):EditTripCostFragment {
             val args = Bundle()
+            args.putString(KEY_SCHEDULE_ID,scheduleId)
             val fragment = EditTripCostFragment()
             fragment.arguments = args
             return fragment
@@ -21,12 +25,15 @@ class EditTripCostFragment: BaseFragment<FragmentAddEstimateCostBinding>() {
     private lateinit var binding: FragmentAddEstimateCostBinding
     private var costTypeAdapter: CostTypesAdapter?= null
     private var listTypeCost: MutableList<TypeCost> = mutableListOf()
+    private var tripViewModel: MyTripsViewModel = MyTripsViewModel()
+    private var scheduleId:String?= null
 
     override fun getLayoutRes(): Int {
         return R.layout.fragment_add_estimate_cost
     }
 
     override fun initView() {
+        scheduleId = arguments?.getString(KEY_SCHEDULE_ID)
         binding = getBinding()
         costTypeAdapter = CostTypesAdapter(requireContext())
         costTypeAdapter?.setSumTotalCostListener(object :CostTypesAdapter.SumTotalCost{
@@ -37,6 +44,8 @@ class EditTripCostFragment: BaseFragment<FragmentAddEstimateCostBinding>() {
         initListType()
         costTypeAdapter?.setListTypeCost(listTypeCost)
         binding.rcvTypeOfCost.adapter = costTypeAdapter
+        tripViewModel.getCostList(scheduleId)
+        tripViewModel.addObserver(this)
 
     }
 
@@ -45,7 +54,6 @@ class EditTripCostFragment: BaseFragment<FragmentAddEstimateCostBinding>() {
         listTypeCost.add(TypeCost(R.drawable.ic_type_visiting,getString(R.string.type_visiting)))
         listTypeCost.add(TypeCost(R.drawable.ic_type_shopping,getString(R.string.type_shopping)))
         listTypeCost.add(TypeCost(R.drawable.ic_type_eating,getString(R.string.type_transportation)))
-        listTypeCost.add(TypeCost())
     }
 
     override fun initData() {
@@ -59,5 +67,13 @@ class EditTripCostFragment: BaseFragment<FragmentAddEstimateCostBinding>() {
     }
 
     override fun setObserver() {
+    }
+
+    override fun update(o: Observable?, arg: Any?) {
+        if (arg is CostResponse){
+            listTypeCost.addAll(arg.data as MutableList<TypeCost>)
+            listTypeCost.add(TypeCost())
+            costTypeAdapter?.setListTypeCost(listTypeCost)
+        }
     }
 }
