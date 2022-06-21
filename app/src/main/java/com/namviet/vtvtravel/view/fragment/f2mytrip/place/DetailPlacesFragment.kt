a62040e8f8761c2f3a469b026161da3ec4991316
hainv <hainv@namviet-corp.com.vn>
2022-06-21 11:03:49 +0700

    add companion object function newInstance

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/place/DetailPlacesFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/place/DetailPlacesFragment.kt
@@ -1,10 +1,25 @@
 package com.namviet.vtvtravel.view.fragment.f2mytrip.place
 
+import android.os.Bundle
 import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.databinding.FragmentDetailSchedulePlacesBinding
 import com.namviet.vtvtravel.f2base.base.BaseFragment
+import com.namviet.vtvtravel.view.fragment.f2mytrip.model.TripItem
 
 class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>() {
+
+    companion object {
+        const val KEY_DETAIL_PLACE_TRIP_ITEM = "trip_item_place"
+
+        fun newInstance(tripItem: TripItem?): DetailPlacesFragment {
+            val args = Bundle()
+            args.putParcelable(KEY_DETAIL_PLACE_TRIP_ITEM,tripItem)
+            val fragment = DetailPlacesFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
     override fun getLayoutRes(): Int {
         return R.layout.fragment_detail_schedule_places
     }
