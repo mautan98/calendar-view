9b46db9642b234a382f88779d729625a2f79447b
hainv <hainv@namviet-corp.com.vn>
2022-06-21 08:29:12 +0700

    add empty detail place fragment

---
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/place/DetailPlacesFragment.kt
@@ -0,0 +1,31 @@
+package com.namviet.vtvtravel.view.fragment.f2mytrip.place
+
+import com.namviet.vtvtravel.R
+import com.namviet.vtvtravel.databinding.FragmentDetailSchedulePlacesBinding
+import com.namviet.vtvtravel.f2base.base.BaseFragment
+
+class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>() {
+    override fun getLayoutRes(): Int {
+        return R.layout.fragment_detail_schedule_places
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun inject() {
+
+    }
+
+    override fun setClickListener() {
+
+    }
+
+    override fun setObserver() {
+
+    }
+}
