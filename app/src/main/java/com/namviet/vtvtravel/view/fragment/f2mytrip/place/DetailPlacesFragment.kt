e65485e3d67bf126b858a69bfb26ad288e734502
hainv <hainv@namviet-corp.com.vn>
2022-06-21 17:07:33 +0700

    set data to adapter

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/place/DetailPlacesFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/place/DetailPlacesFragment.kt
@@ -5,6 +5,7 @@ import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.databinding.FragmentDetailSchedulePlacesBinding
 import com.namviet.vtvtravel.f2base.base.BaseFragment
 import com.namviet.vtvtravel.view.fragment.f2mytrip.model.TripItem
+import com.namviet.vtvtravel.view.fragment.f2mytrip.place.adapter.PlacesAdapter
 import com.namviet.vtvtravel.view.fragment.f2mytrip.place.model.DetailPlacesResponse
 import com.namviet.vtvtravel.view.fragment.f2mytrip.place.model.PlacesScheduleItem
 import com.namviet.vtvtravel.view.fragment.f2mytrip.viewmodel.MyTripsViewModel
@@ -26,6 +27,7 @@ class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>()
     private var tripItem:TripItem? = null
     private var viewModel:MyTripsViewModel? =null
     private var listPlaces:MutableList<PlacesScheduleItem> = mutableListOf()
+    private var adapter:PlacesAdapter? = null
 
     override fun getLayoutRes(): Int {
         return R.layout.fragment_detail_schedule_places
@@ -34,6 +36,9 @@ class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>()
     override fun initView() {
         tripItem = arguments?.getParcelable(KEY_DETAIL_PLACE_TRIP_ITEM)
         viewModel = MyTripsViewModel()
+        adapter = PlacesAdapter()
+        adapter?.setListDetailPlaces(listPlaces)
+        binding.rcvPlaces.adapter = adapter
         tripItem?.id?.let { viewModel?.getDetailPlaces(it) }
         viewModel?.addObserver(this)
     }
@@ -57,5 +62,6 @@ class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>()
     override fun update(o: Observable?, arg: Any?) {
         val response = arg as DetailPlacesResponse
         listPlaces = response.data?.content as MutableList<PlacesScheduleItem>
+        adapter?.setListDetailPlaces(listPlaces)
     }
 }
