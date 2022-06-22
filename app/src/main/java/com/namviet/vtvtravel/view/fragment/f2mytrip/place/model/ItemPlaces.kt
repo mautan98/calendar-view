27853bd2e9a4b60490c6dabc12941e37f09a65e5
hainv <hainv@namviet-corp.com.vn>
2022-06-22 11:23:33 +0700

    add itemPlaces pojo object

---
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/place/model/ItemPlaces.kt
@@ -0,0 +1,15 @@
+package com.namviet.vtvtravel.view.fragment.f2mytrip.place.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ItemPlaces(
+
+    @field:SerializedName("day")
+    var day: Long? = null,
+
+    @field:SerializedName("totalPlace")
+    var totalPlace: Int? = null,
+
+    @field:SerializedName("schedulePlaceList")
+    var schedulePlaceList: MutableList<PlacesScheduleItem>? = null
+)
