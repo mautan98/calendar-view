bc8b12d5084fa5b3d94fedcee25e548486c74990
hainv <hainv@namviet-corp.com.vn>
2022-06-27 08:30:57 +0700

    add companion object

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/InviteFriendScheduleFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/InviteFriendScheduleFragment.kt
@@ -1,5 +1,6 @@
 package com.namviet.vtvtravel.view.fragment.f2mytrip
 
+import android.os.Bundle
 import android.text.TextUtils
 import com.namviet.vtvtravel.R
 import com.namviet.vtvtravel.databinding.FragmentInviteScheduleBinding
@@ -7,6 +8,17 @@ import com.namviet.vtvtravel.f2base.base.BaseFragment
 import com.namviet.vtvtravel.ultils.ValidateUtils
 
 class InviteFriendScheduleFragment : BaseFragment<FragmentInviteScheduleBinding>() {
+
+    companion object {
+        const val KEY_SCHEDULE_ID = "scheduleId"
+        fun newInstance(scheduleId:String): InviteFriendScheduleFragment {
+            val args = Bundle()
+            args.putString(KEY_SCHEDULE_ID,scheduleId)
+            val fragment = InviteFriendScheduleFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
 
     override fun getLayoutRes(): Int {
         return R.layout.fragment_invite_schedule
