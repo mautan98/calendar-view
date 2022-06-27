c3bda57e2076fd06ccbdcee40c40fb7d29416526
hainv <hainv@namviet-corp.com.vn>
2022-06-27 14:03:33 +0700

    implement update arrival time in fragment

--- app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/place/DetailPlacesFragment.kt
+++ app/src/main/java/com/namviet/vtvtravel/view/fragment/f2mytrip/place/DetailPlacesFragment.kt
@@ -3,6 +3,7 @@ package com.namviet.vtvtravel.view.fragment.f2mytrip.place
 import android.os.Bundle
 import android.view.View
 import com.namviet.vtvtravel.R
+import com.namviet.vtvtravel.Utils
 import com.namviet.vtvtravel.databinding.FragmentDetailSchedulePlacesBinding
 import com.namviet.vtvtravel.f2base.base.BaseFragment
 import com.namviet.vtvtravel.response.BaseResponse
@@ -13,6 +14,7 @@ import com.namviet.vtvtravel.view.fragment.f2mytrip.model.TripItem
 import com.namviet.vtvtravel.view.fragment.f2mytrip.place.adapter.AllPlacesAdapter
 import com.namviet.vtvtravel.view.fragment.f2mytrip.place.model.DetailPlacesResponse
 import com.namviet.vtvtravel.view.fragment.f2mytrip.place.model.ItemPlaces
+import com.namviet.vtvtravel.view.fragment.f2mytrip.viewmodel.DetailPlaceScheduleViewModel
 import com.namviet.vtvtravel.view.fragment.f2mytrip.viewmodel.MyTripsViewModel
 import com.namviet.vtvtravel.view.fragment.f2travelvoucher.AlreadyReceiverDialog
 import java.util.*
@@ -32,6 +34,7 @@ class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>()
     }
     private var tripItem:TripItem? = null
     private var viewModel:MyTripsViewModel? =null
+    private var detailPlaceViewModel:DetailPlaceScheduleViewModel? =null
     private var listPlaces:MutableList<ItemPlaces> = mutableListOf()
     private var adapter:AllPlacesAdapter? = null
 
@@ -42,6 +45,7 @@ class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>()
     override fun initView() {
         tripItem = arguments?.getParcelable(KEY_DETAIL_PLACE_TRIP_ITEM)
         viewModel = MyTripsViewModel()
+        detailPlaceViewModel = DetailPlaceScheduleViewModel()
         adapter = AllPlacesAdapter()
         adapter?.setOnItemClickListener(object :AllPlacesAdapter.OnItemClickPlace{
             override fun onItemClick(currentPosition:Int, itemDetailPosition: Int, view: View) {
@@ -54,8 +58,10 @@ class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>()
                                 calendar.timeInMillis = listPlaces[currentPosition].day!!
                                 calendar.set(Calendar.HOUR_OF_DAY,hour)
                                 calendar.set(Calendar.MINUTE,minute)
-                                listPlaces.get(currentPosition).schedulePlaceList?.get(itemDetailPosition)?.arrivalTime = calendar.timeInMillis
-                                adapter?.notifyDataSetChanged()
+                                val arrivalTime = Utils.formatTimeStamp(calendar.timeInMillis,"dd-MM-yyyy HH:mm:ss")
+                                val placeItem = listPlaces.get(currentPosition).schedulePlaceList?.get(itemDetailPosition)
+                                placeItem?.arrivalTime = calendar.timeInMillis
+                                placeItem?.id?.let { detailPlaceViewModel?.updateArrivalTime(it,arrivalTime) }
                             }
                         })
                         dialog.show(childFragmentManager,null)
@@ -78,12 +84,9 @@ class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>()
                         val dialog = BottomWheelDialog()
                         dialog.setOnSaveListener(object : BottomWheelDialog.OnClickSaveWheelTime{
                             override fun onClickSave(hour: Int, minute: Int) {
-                                val calendar = Calendar.getInstance()
-                                calendar.timeInMillis = listPlaces[currentPosition].day!!
-                                calendar.set(Calendar.HOUR_OF_DAY,hour)
-                                calendar.set(Calendar.MINUTE,minute)
-//                                listPlaces.get(currentPosition).schedulePlaceList?.get(itemDetailPosition)?.freeTime = calendar.timeInMillis
-                                adapter?.notifyItemChanged(currentPosition)
+                                val placeItem = listPlaces.get(currentPosition).schedulePlaceList?.get(itemDetailPosition)
+                                placeItem?.freeTime = minute
+                                placeItem?.id?.let { detailPlaceViewModel?.updateFreeTime(it,minute.toString()) }
                             }
                         })
                         dialog.show(childFragmentManager,null)
@@ -95,6 +98,7 @@ class DetailPlacesFragment : BaseFragment<FragmentDetailSchedulePlacesBinding>()
         binding.rcvPlaces.adapter = adapter
         tripItem?.id?.let { viewModel?.getDetailPlaces(it) }
         viewModel?.addObserver(this)
+        detailPlaceViewModel?.addObserver(this)
     }
 
     override fun initData() {
