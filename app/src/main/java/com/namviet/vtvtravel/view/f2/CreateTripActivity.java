ad6860380fc958a00e8b0086d6bc436df9a2fec4
TheUniverse779 <thanhnd7779@gmail.com>
2022-07-17 22:20:39 +0700

    Optimization UI

--- app/src/main/java/com/namviet/vtvtravel/view/f2/CreateTripActivity.java
+++ app/src/main/java/com/namviet/vtvtravel/view/f2/CreateTripActivity.java
@@ -2,6 +2,8 @@ package com.namviet.vtvtravel.view.f2;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.view.View;
 
 import com.namviet.vtvtravel.R;
 import com.namviet.vtvtravel.config.Constants;
@@ -44,5 +46,13 @@ public class CreateTripActivity extends BaseActivityNew {
     public static void startScreen(Context activity){
         Intent intent = new Intent(activity, CreateTripActivity.class);
         activity.startActivity(intent);
+    }
+
+    @Override
+    public void afterSetContentView() {
+        super.afterSetContentView();
+        getWindow().getDecorView().setSystemUiVisibility (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN| View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
 }
